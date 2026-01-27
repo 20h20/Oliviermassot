3c7ea22e5a40b22d1728e99287acad94fd3e2b9b
JulienB <brochard.ju@gmail.com>
2026-01-27 16:10:41 +0100

    theme added

---
+++ Gruntfile.js
@@ -0,0 +1,216 @@
+module.exports = function (grunt) {
+  require("time-grunt")(grunt);
+  require("jit-grunt")(grunt);
+
+  grunt.initConfig({
+    site_url: "http://localhost:8888/olivierm/web",
+    theme_name: "rentrust",
+
+    /* Chemin pour les blocks */
+    path_src_blocks: "web/app/themes/olivierm/templates/blocks/",
+    path_dist_blocks: "web/app/themes/olivierm/library/css/blocks/",
+
+    /* Chemin pour les parts (NOUVEAU) */
+    path_src_parts: "web/app/themes/olivierm/templates/parts/",
+    path_dist_parts: "web/app/themes/olivierm/library/css/parts/",
+
+    /* Chemin pour les styles globaux */
+    path_src_styles: "web/app/themes/olivierm/src/scss/",
+    path_dist_styles: "web/app/themes/olivierm/library/css/",
+
+    /* Chemin pour les js et icons */
+    path_src: "web/app/themes/olivierm/src/",
+    path_dist: "web/app/themes/olivierm/library/",
+    pkg: grunt.file.readJSON("package.json"),
+
+    /* ------------------------ */
+    /*   SASS : Blocks & Parts  */
+    /* ------------------------ */
+
+    sass: {
+      blocks: {
+        expand: true,
+        cwd: "<%= path_src_blocks %>",
+        src: ["**/style.scss", "!style.scss"],
+        dest: "<%= path_dist_blocks %>",
+        rename: function (dest, src) {
+          var name = src.split("/")[src.split("/").length - 2];
+          return dest + name + ".min.css";
+        },
+        options: {
+          implementation: require("node-sass"),
+          sourceMap: false
+        }
+      },
+
+      parts: {
+        expand: true,
+        cwd: "<%= path_src_parts %>",
+        src: ["**/style.scss", "!style.scss"],
+        dest: "<%= path_dist_parts %>",
+        rename: function (dest, src) {
+          var name = src.split("/")[src.split("/").length - 2];
+          return dest + name + ".min.css";
+        },
+        options: {
+          implementation: require("node-sass"),
+          sourceMap: false
+        }
+      },
+
+      global: {
+        src: "<%= path_src_styles %>style.scss",
+        dest: "<%= path_dist_styles %>style.min.css",
+        options: { sourceMap: false }
+      }
+    },
+
+    /* ------------------------ */
+    /*   POSTCSS : Blocks/Parts */
+    /* ------------------------ */
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require("autoprefixer")({ overrideBrowserslist: "last 3 versions" }),
+          require("cssnano")()
+        ]
+      },
+
+      blocks: {
+        src: "<%= path_dist_blocks %>*.css"
+      },
+
+      /* 🔥 NOUVEAU : minification des CSS des parts */
+      parts: {
+        src: "<%= path_dist_parts %>*.css"
+      },
+
+      global: {
+        src: "<%= path_dist_styles %>style.min.css",
+        dest: "<%= path_dist_styles %>style.min.css"
+      }
+    },
+
+    /* ------------------------ */
+    /*    GUTENBERG MERGE CSS   */
+    /* ------------------------ */
+    concat: {
+      options: { separator: ";" },
+      gutenberg: {
+        src: ["<%= path_dist_blocks %>*.css"],
+        dest: "<%= path_dist_styles %>gutenberg.min.css"
+      },
+      dist: {
+        src: ["<%= path_src %>js/**/*.js"],
+        dest: "<%= path_dist %>js/scripts.js"
+      }
+    },
+
+    /* ------------------------ */
+    /*       ICON FONT          */
+    /* ------------------------ */
+    webfont: {
+      icons: {
+        src: "<%= path_src %>icons/*.svg",
+        dest: "<%= path_dist %>iconfont",
+        destCss: "<%= path_src %>scss/partials/",
+        options: {
+          stylesheet: "scss",
+          relativeFontPath: "../iconfont",
+          template: "<%= path_src %>scss/partials/_icons-template.scss",
+          types: "eot,woff,ttf,svg",
+          htmlDemo: false,
+          optimize: false,
+          engine: "node",
+          autoHint: false
+        }
+      }
+    },
+
+    /* ------------------------ */
+    /*      JS BUILD            */
+    /* ------------------------ */
+    include_file: {
+      default_options: {
+        cwd: "<%= path_src %>js/",
+        src: ["scripts.js"],
+        dest: "<%= path_dist %>js/"
+      }
+    },
+
+    uglify: {
+      app: {
+        files: {
+          "<%= path_dist %>js/scripts.js": ["<%= path_dist %>js/scripts.js"]
+        }
+      }
+    },
+
+    /* ------------------------ */
+    /*       WATCH TASKS        */
+    /* ------------------------ */
+    watch: {
+      options: { spawn: false },
+
+      iconfont: {
+        files: "<%= path_src %>icons/*.svg",
+        tasks: ["webfont"]
+      },
+
+      blocks: {
+        files: ["<%= path_src_blocks %>**/*.scss"],
+        tasks: ["sass:blocks", "postcss:blocks", "concat:gutenberg"]
+      },
+
+      /* 🔥 NEW : watch des parts */
+      parts: {
+        files: ["<%= path_src_parts %>**/*.scss"],
+        tasks: ["sass:parts", "postcss:parts"]
+      },
+
+      global: {
+        files: ["<%= path_src %>**/*.scss"],
+        tasks: ["sass:global", "postcss:global"]
+      },
+
+      js: {
+        files: "<%= path_src %>js/**/*.js",
+        tasks: ["include_file", "uglify"]
+      }
+    }
+  });
+
+  /* ------------------------ */
+  /*     LOAD TASKS           */
+  /* ------------------------ */
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-postcss");
+
+  /* ------------------------ */
+  /*       DEFAULT            */
+  /* ------------------------ */
+  grunt.registerTask("default", [
+    "sass:blocks", "sass:parts", "sass:global",
+    "postcss:blocks", "postcss:parts", "postcss:global",
+    "concat:gutenberg",
+    "webfont",
+    "include_file",
+    "uglify",
+    "watch"
+  ]);
+
+  /* ------------------------ */
+  /*        BUILD             */
+  /* ------------------------ */
+  grunt.registerTask("build", [
+    "sass:blocks", "sass:parts", "sass:global",
+    "postcss:blocks", "postcss:parts", "postcss:global",
+    "concat:gutenberg",
+    "webfont",
+    "include_file",
+    "uglify"
+  ]);
+};
